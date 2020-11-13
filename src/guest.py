23db216f44a8aa25d195dc2674ab5013cdb5e21f
William Anderson <willjatlas@googlemail.com>
2020-11-13 15:02:56 +0000

    Setup for mvp

---
+++ src/guest.py
@@ -0,0 +1,9 @@
+# Class definition to a guest object.
+
+class Guest:
+
+    def __init__(self, name, age, wallet):
+        self.name = name 
+        self.age = age 
+        self.wallet = wallet
+
